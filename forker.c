ed0e3eca5cba622bb656c547cff0da71238a6df4
Todd Jackson, II <wtjackson314@gmail.com>
2021-03-14 15:29:44 -0400

    Moved logic containing the wait function from the mourn function to the main function. Also uncommented the pid_to_name function. Utilized the pid_to_name function to get the name of each process, instead of hard setting it when each process is declared.

--- forker.c
+++ forker.c
@@ -14,32 +14,68 @@ void greet(char myName)
 
 void goaway(char myName)
 {
-	
-	printf("Process %d exited normally.\n", myName);
+	printf("Process %c exited normally.\n", myName);
 	exit(0);
 }
 
-void mourn(char parName, char childName){
+void mourn(char parent_name, char child_name, int status){
 
-	int status;
-    pid_t result;
-	wait(&status);
 	if (WIFEXITED(status)){
-		printf("Process %d here: Process %d exited with status %d.\n", parName, childName, WEXITSTATUS(status));
+		printf("Process %c here: Process %c exited with status %d.\n", parent_name, child_name, WEXITSTATUS(status));
 	}
 }
 
 
-// char pid_to_name(pid_t base_id, pid_t proc_id){
+char pid_to_name(pid_t base_id, pid_t proc_id){
 	
-// 	char names[] = {'A','B','C','D'};
-// 	int i = (base_id - proc_id);
+	char names[] = {'A','B','C','D'};
+	int i = (base_id - proc_id);
 
-// 	return names[i];
-// } 
+	return names[i];
+} 
 
 int main()
 {
+	pid_t base_pid = getpid();
+	pid_t pid;
+	int status;
+	char curr_name;
+
+	curr_name = pid_to_name(base_pid, getpid());
+	greet(curr_name);
+
+	if ((pid = fork()) == 0) {
+		
+		curr_name = pid_to_name(base_pid, getpid());
+		greet(curr_name);
+		
+		pid = fork();
+		
+		if (pid == 0) {
+
+			curr_name = pid_to_name(base_pid, getpid());
+			greet(curr_name);
+			goaway(curr_name);
+		}
+
+		pid_t child_pid = wait(&status);
+		char child_name = pid_to_name(base_pid, child_pid);
+		mourn(curr_name, child_name);
+		goaway(curr_name);
+	}
+
+	if ((pid = fork()) == 0) {
+
+		curr_name = pid_to_name(base_pid, getpid());
+		greet(curr_name);
+		goaway(curr_name);
+		
+	}
+ 
+	pid_t child_pid = wait(&status);
+	char child_name = pid_to_name(base_pid, child_pid);
+	mourn(curr_name, child_name);
+	goaway(curr_name);
 
   return 0;
 }
